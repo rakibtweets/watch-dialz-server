bebce8b77079f16ee80ab9cc9e76ed84a55b5b5c
Rakib Hasan <rakibhasanroky0@gmail.com>
2024-01-03 18:50:40 +0600

    error:cors index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  }
+  
+  ));
 app.use(express.json());
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ytnhs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
